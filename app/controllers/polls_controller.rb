024f60d6ec44aa90230fd1de0077e8b7765308dc
kian-weimer <55513580+kian-weimer@users.noreply.github.com>
2021-11-16 19:58:13 -0600

    created new poll creation controller method.

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -8,11 +8,25 @@ class PollsController < ApplicationController
   end
 
   def new
-
   end
 
   def create
+    poll_info = params[:poll]
+    poll = Poll.create({title: poll_info['title'],
+                        description: poll_info['description'],
+                        location: poll_info['location'],
+                        votes_per_user: poll_info['user_votes'],
+                        votes_per_timeslot: poll_info['votes_per_timeslot'],
+                        timezone: poll_info['timezone']
+                       })
 
+    unless poll.valid?
+      flash[:warning] = ""
+      poll.errors.keys.each do |key|
+        flash[:warning] = flash[:warning] + "#{key} #{ poll.errors[key].first}; "
+      end
+      return redirect_to "/polls/new"
+    end
   end
 
   def edit
