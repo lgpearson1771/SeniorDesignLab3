286aab654434ee1e03293bc96152ac3ac61983e8
Luke Pearson <l.pearson.vzw@gmail.com>
2021-11-16 21:39:34 -0600

    Include the added columns within the polls relation.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,5 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Admin.create(id: 1, username: 'lukegpearson', password: '12345')
-Poll.create(id: 1, title: 'test_poll', description: 'Huehnergarth is hard to spell', votes_per_user: 4, votes_per_timeslot: 2, admin_id: 1)
+Poll.create(id: 1, title: 'test_poll', description: 'Huehnergarth is hard to spell',
+            votes_per_user: 4, votes_per_timeslot: 2, admin_id: 1, timezone: 'CDT', location: 'Seamans Center')
