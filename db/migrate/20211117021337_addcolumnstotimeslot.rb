3308cb099b185cc420f94064401432f1da9005fd
kian-weimer <55513580+kian-weimer@users.noreply.github.com>
2021-11-21 13:28:47 -0600

    RYS- Fixed poll databases.

--- db/migrate/20211117021337_addcolumnstotimeslot.rb
+++ db/migrate/20211117021337_addcolumnstotimeslot.rb
@@ -1,6 +1,6 @@
 class Addcolumnstotimeslot < ActiveRecord::Migration
   def change
-    add_reference :timeslot, :polls, index: true
+    add_reference :timeslot, :poll, index: true
     add_column :timeslot, :start, :string
     add_column :timeslot, :end, :string
     add_column :timeslot, :day, :date
--- db/migrate/20211117024748_addcolumnstoblocks.rb
+++ db/migrate/20211117024748_addcolumnstoblocks.rb
@@ -1,6 +1,6 @@
 class Addcolumnstoblocks < ActiveRecord::Migration
   def change
-    add_reference :blocks, :timeslots, index: true
+    add_reference :blocks, :timeslot, index: true
     add_column :blocks, :start, :string
     add_column :blocks, :end, :string
   end
