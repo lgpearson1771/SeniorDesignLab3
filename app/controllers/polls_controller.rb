cd53d5c96ddcb81db5a08beef2be90642a2e5618
root <rys-huehnergarth@uiowa.edu>
2021-11-21 13:35:44 -0600

    adding fields to edit file

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -67,15 +67,4 @@ class PollsController < ApplicationController
     redirect_to "/polls/#{@poll.id}/edit?meetings=true"
   end
 
-  def add_times
-    require 'time'
-
-    start_time = Time.parse(params['start']) #=> 2010-10-31 00:00:00 -0500
-    start_minutes = start_time.strftime('%M')
-
-    end_time = Time.parse(params['end'])
-    end_minutes = end_time.strftime('%M')
-
-    redirect_to "/polls/#{params[:id]}/edit?meetings=true"
-  end
 end
