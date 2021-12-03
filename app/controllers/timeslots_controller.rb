bb22eea740cebd09d7d4a773fd5b8e5555d0e725
rhuehnergarth <56234481+rhuehnergarth@users.noreply.github.com>
2021-12-03 10:05:11 -0600

    fixed block creation to fix a bug

--- app/controllers/timeslots_controller.rb
+++ app/controllers/timeslots_controller.rb
@@ -11,15 +11,20 @@ class TimeslotsController < ApplicationController
     start_mins_string = "0" + start_mins_string if start_mins_string.length == 1
     end_mins_string = "0" + start_mins_string if start_mins_string.length == 1
 
+    start_hour_string = params['start']['start_time(4i)']
+    end_hour_string = params['end']['end_time(4i)']
+    start_hour_string = "0" + start_hour_string if start_hour_string.length == 1
+    end_hour_string = "0" + end_hour_string if end_hour_string.length == 1
+
     if params["AM_PM_start"]['select'] == "PM"
       timeslot_start_time = (start_hour + 12).to_s + ":" + start_mins_string
       start = start_hour.to_i + 12
     else
-      timeslot_start_time = params['start']['start_time(4i)'] + ":" + start_mins_string
+      timeslot_start_time = start_hour_string + ":" + start_mins_string
       start = start_hour
       if start_hour == 12
         start = start - 12
-        timeslot_start_time = (params['start']['start_time(4i)'].to_i - 12).to_s + ":" + start_mins_string
+        timeslot_start_time = "0" + (start_hour - 12).to_s + ":" + start_mins_string # 00:xx
       end
     end
 
@@ -27,12 +32,11 @@ class TimeslotsController < ApplicationController
       timeslot_end_time = (end_hour + 12).to_s + ":" + end_mins_string
       end_time =  end_hour + 12
     else
-      timeslot_end_time = params['end']['end_time(4i)'] + ":" + end_mins_string
+      timeslot_end_time = end_hour_string + ":" + end_mins_string
       end_time =  end_hour
     end
-
     total_minutes = (end_time - start) * 60 - start_minute + end_minute
-    print "\n\n total:#{total_minutes}  start hr: #{start}start min:#{start_minute} end hr: #{end_time} end min:#{end_minute}"
+
     if params['time_blocks']['select'] == 'blocks'
       interval = (total_minutes / params[:timeslot][:blocks].to_i)
     else
@@ -59,13 +63,15 @@ class TimeslotsController < ApplicationController
     day = Date.strptime(params[:date], '%Y-%m-%d')
     timeslot = Timeslot.create(day: day, end: timeslot_end_time, start: timeslot_start_time, poll_id: params[:poll_id])
 
+    print(start)
     (0..(total_minutes - interval)).step(interval) do |number|
       start_first = number / 60 + start
       start_second = (number + start_minute) % 60
       end_first = (number + interval) / 60 + start
       end_second = ((number + interval) + start_minute) % 60
-      start_first = start_first - 12 if start_first > 12
-      end_first = end_first - 12 if end_first > 12
+
+      start_first = "0" + start_first.to_s if start_first.to_s.length == 1
+      end_first = "0" + end_first.to_s if end_first.to_s.length == 1
       start_second = "0" + start_second.to_s if start_second.to_s.length == 1
       end_second = "0" + end_second.to_s if end_second.to_s.length == 1
 
